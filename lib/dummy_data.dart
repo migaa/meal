2db68bd1cc1f78805f3876ba751fcbe7f24ac9a0
migaa <tsmigaa@gmail.com>
2020-12-07 22:06:18 +0800

    l61

---
+++ lib/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Итали',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Хөнгөн хоол',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Гамбургер',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Герман',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Зууш',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Өвөрмөц хоол',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Өглөөний цай',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Ази',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Франц',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Хээрийн хоол',
+    color: Colors.teal,
+  ),
+];
