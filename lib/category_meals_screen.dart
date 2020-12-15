10d24d0d3be0ff4c91729c9f2e3938aef7c77b52
migaa <tsmigaa@gmail.com>
2020-12-16 01:00:39 +0800

    l65

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categotyTitle;
+  // final String categoryId;
+  // final String categotyTitle;
 
-  CategoryMealsScreen(this.categoryId, this.categotyTitle);
+  // CategoryMealsScreen(this.categoryId, this.categotyTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(categotyTitle),
+        title: Text(categoryTitle),
       ),
       body: Text('Хоолны жагсаалт'),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() {
@@ -31,6 +32,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
