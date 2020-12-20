f9d06c07040d56c4d5c8176ec9e7e0c6c2d9eac9
migaa <tsmigaa@gmail.com>
2020-12-20 10:00:24 +0800

    l68

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './models/Meal.dart';
-import 'dummy_data.dart';
+import 'package:meal/widgets/meal_item.dart';
+import '../models/Meal.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/categort-meals';
@@ -25,7 +26,13 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
         },
         itemCount: categoryMeals.length,
       ),
