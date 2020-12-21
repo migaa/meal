51b80204d65e8749d3c2114163a70a0f32240d94
migaa <tsmigaa@gmail.com>
2020-12-21 23:08:18 +0800

    l69

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -16,6 +16,38 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Хялбар';
+        break;
+      case Complexity.Challenging:
+        return 'Хэцүү';
+        break;
+      case Complexity.Hard:
+        return 'Хүнд';
+        break;
+      default:
+        return 'Тодорхойгүй';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Боломжын';
+        break;
+      case Affordability.Pricey:
+        return 'Yнэтэй';
+        break;
+      case Affordability.Luxurious:
+        return 'Тансаг';
+        break;
+      default:
+        return 'Тодорхойгүй';
+    }
+  }
+
   void selectmeal() {}
 
   @override
@@ -43,9 +75,65 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration мин'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
