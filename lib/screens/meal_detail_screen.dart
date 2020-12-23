48b2516d6b2b04e7b41c33648655da5e287bacfe
migaa <tsmigaa@gmail.com>
2020-12-23 21:24:19 +0800

    l70

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context).settings.arguments as String;
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(mealId),
+        ),
+        body: Text('$mealId Хоолны дэлгэрэнгүй'),
+      ),
+    );
+  }
+}
