08510b3316756bc425c12bfe6cfa7e3ec709f3e7
migaa <tsmigaa@gmail.com>
2020-12-29 19:36:38 +0800

    76

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const routName = '/filters';
+
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  bool _gluttenFree = false;
+  bool _vegeterian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  Widget _buildSwitchListTile(
+    String title,
+    String description,
+    bool currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +35,51 @@ class FilterScreen extends StatelessWidget {
         title: Text('Хайлт'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Хайлт'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Center(
+              child: Text(
+                'Хоолны тохиргоо',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildSwitchListTile(
+                    'Цавуулаг багатай', 'Зөвхөн цавуулаггүй хоол', _gluttenFree,
+                    (newValue) {
+                  setState(() {
+                    _gluttenFree = newValue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Ногоон хоол', 'Зөвхөн ногоон хоол', _vegan, (newValue) {
+                  setState(() {
+                    _vegan = newValue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Цагаан хоол', 'Зөвхөн цагаан хоол', _vegeterian,
+                    (newValue) {
+                  setState(() {
+                    _vegeterian = newValue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Лактозгүй', 'Зөвхөн лактозгүй хоол', _lactoseFree,
+                    (newValue) {
+                  setState(() {
+                    _lactoseFree = newValue;
+                  });
+                })
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
